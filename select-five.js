e75f33695f3f5e3843bb24ac9c9bdde68dd7198c
sampod76 <sampodnath76@gmail.com>
2022-08-22 05:04:20 +0600

    add alart not a namber

--- select-five.js
+++ select-five.js
@@ -11,15 +11,15 @@ function getInputFuildById(input) {
 function getElementTextById(inputElementId) {
     const getElement = document.getElementById(inputElementId);
     const getElementString = getElement.innerText;
-    const getElementNumber=parseInt(getElementString)
+    const getElementNumber = parseInt(getElementString)
     return getElementNumber
 
 }
 
 // set element by id 
-function setElementById(inputSetElementId,value){
-    const setElementValue=document.getElementById(inputSetElementId);
-    setElementValue.innerText=value;
+function setElementById(inputSetElementId, value) {
+    const setElementValue = document.getElementById(inputSetElementId);
+    setElementValue.innerText = value;
 
 }
 
@@ -35,29 +35,39 @@ function selectPlayer(playerId) {
         creaiteLiTage.innerText = getPlayerName
         const getOlTage = document.getElementById("selectList")
         getOlTage.appendChild(creaiteLiTage)
-    
+
     }
 }
 
 // calclulation player cost
-document.getElementById("btn-Calculate").addEventListener("click",function(){
-    const getPerPlayerCost=getInputFuildById("Per-Player-fuild")
-    const orderlist = document.querySelectorAll("#selectList li").length
-    const allPlayerTotalCost=getPerPlayerCost*orderlist
-    setElementById("Player-Expenses",allPlayerTotalCost)
+document.getElementById("btn-Calculate").addEventListener("click", function () {
+    const getPerPlayerCost = getInputFuildById("Per-Player-fuild")
+    if (isNaN(getPerPlayerCost)) {
+        alert("Please enter the number")
+    }
+    else {
+        const orderlist = document.querySelectorAll("#selectList li").length
+        const allPlayerTotalCost = getPerPlayerCost * orderlist
+        setElementById("Player-Expenses", allPlayerTotalCost)
+    }
 
     // console.log(allPlayerTotalCost)
 
 })
 
 // calclulation total play in the cost 
-document.getElementById("btn-Calculate-total").addEventListener("click",function(){
-    const getTotalCost=getElementTextById("Player-Expenses");
-    const getManagerfuildValue=getInputFuildById("Manager-fuild");
-    const getCoachfuildValue=getInputFuildById("Coach-fuild");
-    const totalAmount=getTotalCost+getManagerfuildValue+getCoachfuildValue;
-    setElementById("Total-balance",totalAmount)
-    
+document.getElementById("btn-Calculate-total").addEventListener("click", function () {
+    const getTotalCost = getElementTextById("Player-Expenses");
+    const getManagerfuildValue = getInputFuildById("Manager-fuild");
+    const getCoachfuildValue = getInputFuildById("Coach-fuild");
+    if (isNaN(getManagerfuildValue) || isNaN(getCoachfuildValue)) {
+        alert("Please enter the number")
+    }
+    else {
+        const totalAmount = getTotalCost + getManagerfuildValue + getCoachfuildValue;
+        setElementById("Total-balance", totalAmount)
+    }
+
 })
 
 
